a7b3ac51c3fa4084fb60fd528f3e920f45699353
jenujose <jenuster@gmail.com>
2015-01-07 09:57:19 -0500

    Update Application Version Distribution Points
    This script increments software version for SCCM 2012 Applications.

---
+++ UpdateApplicationsonDP.ps1
@@ -0,0 +1,19 @@
+﻿ Import-Module 'C:\Program Files (x86)\Microsoft Configuration Manager\bin\ConfigurationManager.psd1'
+CD FVE:
+$applications=Get-CMApplication |Select-Object ApplicationName,LocalizedDisplayName, CIVersion |Where-Object CIVersion -eq "1" 
+foreach ($app in $applications)
+{
+$app=$app.LocalizedDisplayName
+Write-host "Application :" "$app" -ForegroundColor DarkCyan
+
+$dpttypes=Get-CMDeploymentType -ApplicationName $app
+
+foreach ($dpt in $dpttypes)
+{
+$dptname=$dpt.LocalizedDisplayName
+Write-Host "Deployment Type:" "$dptname" -ForegroundColor DarkGreen
+Update-CMDistributionPoint -ApplicationName $app -DeploymentTypeName $dptname 
+  }
+  }
+
+
